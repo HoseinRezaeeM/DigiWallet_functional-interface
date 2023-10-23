5974a592ab426e9ebb0cd1ba54f56a483105c4d7
HoseinRezaeeM <rezaeen77@gmail.com>
2023-10-23 21:50:49 +0330

    This model contains only one String id

---
+++ src/main/java/digipay/model/Wallet.java
@@ -0,0 +1,14 @@
+package digipay.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@SuppressWarnings("unused")
+@AllArgsConstructor
+@Getter
+@Setter
+public class Wallet {
+    private String id;
+}
