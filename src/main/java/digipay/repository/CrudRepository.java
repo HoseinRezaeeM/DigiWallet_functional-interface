12315dc465f66eb04ec55d09ee42f93153a04eba
HoseinRezaeeM <rezaeen77@gmail.com>
2023-10-23 21:54:40 +0330

    This interface includes two generic parameters T (model type) and ID (model id type).

---
+++ src/main/java/digipay/repository/CrudRepository.java
@@ -0,0 +1,17 @@
+package digipay.repository;
+
+import digipay.model.Transaction;
+
+import java.util.List;
+import java.util.function.Predicate;
+@SuppressWarnings("unused")
+public interface CrudRepository<T extends Transaction, ID extends Long> {
+    boolean add(T t);
+
+    List<T> getAll();
+
+    T get(ID id);
+
+    List<T> get(Predicate<T> predicate);
+
+}
