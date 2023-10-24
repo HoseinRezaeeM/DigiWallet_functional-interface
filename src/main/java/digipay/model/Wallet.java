1d7e8f3cd264912be3713b841594c935299b2719
HoseinRezaeeM <rezaeen77@gmail.com>
2023-10-24 16:21:41 +0330

    delete @ToSting and write toString

--- src/main/java/digipay/model/Wallet.java
+++ src/main/java/digipay/model/Wallet.java
@@ -5,10 +5,17 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+
 @SuppressWarnings("unused")
 @AllArgsConstructor
 @Getter
 @Setter
+
 public class Wallet {
     private String id;
+
+    @Override
+    public String toString() {
+        return "ID =" + id ;
+    }
 }
