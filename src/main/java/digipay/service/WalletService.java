19098f277e6bf45be0b1f0257b898eeb9f0a20ab
HoseinRezaeeM <rezaeen77@gmail.com>
2023-10-24 22:16:27 +0330

    Upon receiving a new transaction and status, return false if the transaction status was not PENDING or the new status was PENDING. Otherwise, if the transaction was of type WITHDRAWAL and the wallet balance was not equal to the transaction amount, an IllegalArgumentException will be thrown. Otherwise, the state of the transaction is changed to the new state, the value of the updatedAt property of the transaction is updated and the value of true is returned.

--- src/main/java/digipay/service/WalletService.java
+++ src/main/java/digipay/service/WalletService.java
@@ -47,21 +47,28 @@ public class WalletService {
                 .filter((a) -> a.getStatus().equals(TransactionStatus.ACCEPTED))
                 .filter((a) -> a.getType().equals(TransactionType.WITHDRAWAL))
                 .map(Transaction::getAmount).reduce(BigDecimal::add);
-        BigDecimal subtract=null;
+        BigDecimal subtract = null;
         if (reduceWidthraw.isPresent()) {
             if (reduceDeposit.isPresent()) {
                 subtract = reduceDeposit.get().subtract(reduceWidthraw.get());
-                if (subtract.intValue()<0){
+                if (subtract.intValue() < 0) {
                     throw new IllegalArgumentException();
                 }
             }
         }
-       return  subtract;
+        return subtract;
     }
 
     public boolean setTransactionStatus(Transaction transaction, TransactionStatus status) {
+        if (transaction.getStatus().equals(TransactionStatus.PENDING) || status.equals(TransactionStatus.PENDING)) {
+            return false;
+        } else if (transaction.getType().equals(TransactionType.WITHDRAWAL) &&
+                getBalance(transaction.getWallet()).intValue() < transaction.getAmount().intValue()) {
+            throw new IllegalArgumentException();
+        } else
+            transaction.setStatus(status);
         return true;
+
+
     }
-
-
 }
