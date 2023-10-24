9baeabb460e88a2a252c6598e6cfe1c796133b00
HoseinRezaeeM <rezaeen77@gmail.com>
2023-10-24 16:42:45 +0330

    You can implement the addTransaction method to add a transaction to the transaction list by calling the method from the TransactionRepository upon receiving a transaction. This method should return false if the transaction already exists and true otherwise. You can implement the getTransactions(wallet wallet) method to return a list of all transactions related to the input wallet in order of insertion.

--- src/main/java/digipay/service/WalletService.java
+++ src/main/java/digipay/service/WalletService.java
@@ -1,29 +1,37 @@
 package digipay.service;
 
 import digipay.model.Transaction;
-import digipay.model.TransactionStatus;
+import digipay.model.enumeration.TransactionStatus;
 import digipay.model.Wallet;
+import digipay.repository.impl.TransactionRepositoryImpl;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 @SuppressWarnings("unused")
 public class WalletService {
+    TransactionRepositoryImpl transactionRepository = new TransactionRepositoryImpl();
 
     public boolean addTransaction(Transaction transaction) {
-        return true;
+        return transactionRepository.add(transaction);
     }
-    public Transaction getTransactions(Wallet wallet){
 
+    public List<Transaction> getTransactions(Wallet wallet) {
+        return transactionRepository.getAll().stream().filter((a)->a.getWallet().equals(wallet)).collect(Collectors.toList()) ;
+    }
+
+    public void getTransactions(Wallet wallet, Predicate<Transaction> predicate) {
+
+    }
+
+    public BigDecimal getBalance(Wallet wallet) {
         return null;
     }
-    public void getTransactions(Wallet wallet, Predicate<Transaction> predicate){
 
-    }
-    public BigDecimal getBalance(Wallet wallet){
-        return null;
-    }
-    public boolean setTransactionStatus(Transaction transaction, TransactionStatus status){
+    public boolean setTransactionStatus(Transaction transaction, TransactionStatus status) {
         return true;
     }
 
