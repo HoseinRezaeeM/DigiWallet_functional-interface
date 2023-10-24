be5942f6deafe7ea0aff0e5b28efab0c61ac50d2
HoseinRezaeeM <rezaeen77@gmail.com>
2023-10-24 15:55:48 +0330

    Add final static transactionList And implemention add(),getAll() Method

--- src/main/java/digipay/repository/impl/TransactionRepositoryImpl.java
+++ src/main/java/digipay/repository/impl/TransactionRepositoryImpl.java
@@ -3,28 +3,37 @@ package digipay.repository.impl;
 import digipay.model.Transaction;
 import digipay.repository.TransactionRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 @SuppressWarnings("unused")
 public class TransactionRepositoryImpl implements TransactionRepository {
+    public final static List<Transaction> transactionList = new ArrayList<>();
 
     @Override
     public boolean add(Transaction transaction) {
-        return false;
+        transactionList.add(transaction);
+        if (transactionList.contains(transaction)) {
+            return false;
+        } else
+            return true;
     }
 
     @Override
     public List<Transaction> getAll() {
-        return null;
+        return transactionList;
     }
 
     @Override
-    public Transaction get(Long aLong) {
-        return null;
+    public List<Transaction> get(Long aLong) {
+
     }
 
     @Override
     public List<Transaction> get(Predicate<Transaction> predicate) {
-        return null;
+
+
     }
 }
