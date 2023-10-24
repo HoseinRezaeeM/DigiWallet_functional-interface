8950954686700e24b9e42a37a1641b6da43f5748
HoseinRezaeeM <rezaeen77@gmail.com>
2023-10-24 16:14:18 +0330

    write List<Transaction> getTransaction(Predicate <Tranaction> predicate) method by stream

--- src/main/java/digipay/service/AdminWalletService.java
+++ src/main/java/digipay/service/AdminWalletService.java
@@ -1,23 +1,22 @@
 package digipay.service;
 
 import digipay.model.Transaction;
-import digipay.model.TransactionStatus;
+import digipay.model.enumeration.TransactionStatus;
+import digipay.repository.impl.TransactionRepositoryImpl;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class AdminWalletService extends WalletService {
+    TransactionRepositoryImpl transactionRepository = new TransactionRepositoryImpl();
 
     public List<Transaction> getTransactions(Predicate<Transaction> predicate) {
-        List<Transaction> transactionList =new ArrayList<>();
-        Transaction transaction =new Transaction();
-        if(transaction.getStatus()==TransactionStatus.PENDING){
-            return transactionList;
-        }
-        return null;
+        return transactionRepository.getAll().stream().filter(predicate).collect(Collectors.toList());
     }
-    public  List<Transaction> getAllTransactions(){
+
+    public List<Transaction> getAllTransactions() {
         return null;
     }
 }
